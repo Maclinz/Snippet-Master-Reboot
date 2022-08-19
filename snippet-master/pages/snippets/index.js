6eee427dd82ef06df37a6d97a1d54327667de142
maclinz <tafadzwamucheke@gmail.com>
2022-08-19 17:39:31 +0100

    select code theme

--- snippet-master/pages/snippets/index.js
+++ snippet-master/pages/snippets/index.js
@@ -15,28 +15,26 @@ function Snippets() {
         return <div>Loading...</div>
     }
 
-    console.log('loading', loading);
-
     return (
         <Layout>
-            <MainContent>
-                <SnippetsStyed theme={theme}>
+            <MainContent >
+                <AllSnippetsStyed theme={theme}>
                     {
                         snippets.map(snippet => {
                             return <Snippet key={snippet._id} snippet={snippet} />
                         })
                     }
-                </SnippetsStyed>
+                </AllSnippetsStyed>
             </MainContent>
         </Layout>
     )
 }
 
-const SnippetsStyed = styled.div`
+const AllSnippetsStyed = styled.div`
     padding: 6rem 1.5rem;
-    grid-gap: ${props => props.theme.gridGap};
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    grid-gap: 2rem;
 `;
 
 export default Snippets
